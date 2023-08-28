3ef3b09cba3c102a4bd2a33c7863724801d828a6
Serikov Pavlo <pavloo.serikov@gmail.com>
2023-08-28 13:16:23 +0300

    deleted View.class

--- src/View.java
+++
@@ -1,9 +0,0 @@
-public class View {
-    public void gamePrint(){
-        System.out.println(" | | ");
-
-    }
-    public void gameStatusPrint(){
-        System.out.println("Player ");
-    }
-}
